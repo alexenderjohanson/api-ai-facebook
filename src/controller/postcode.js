3d15a289fa0bd5121519de7480eec4d2449886b8
Johanson Chew <alexenderjohanson@yahoo.com>
2016-05-16 00:03:07 +0800

    update

--- src/controller/postcode.js
+++ src/controller/postcode.js
@@ -4,12 +4,12 @@ const postcodeJson = require('../data/postcode');
 const _ = require('lodash');
 
 exports.validatePostcode = function (postcode) {
-    
-    let index = _.findIndex(postcodeJson, { "postcode": postcode });
-    
-    if(index < 0){
+
+    let index = _.findIndex(postcodeJson, { "postcode": parseInt(postcode) });
+
+    if (index < 0) {
         return false;
     }
-    
+
     return postcodeJson[index];
 }
