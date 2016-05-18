f34b4ef596af8631110a5a967e4263ab05186238
Johanson Chew <alexenderjohanson@yahoo.com>
2016-05-19 06:49:06 +0800

    update

--- src/controller/cache.js
+++ src/controller/cache.js
@@ -3,7 +3,7 @@
 const NodeCache = require("node-cache");
 const myCache = new NodeCache({ stdTTL: 300, checkperiod: 120 });
 const shortid = require('shortid');
-shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$@');
 
 exports.generateShortId = function () {
 
