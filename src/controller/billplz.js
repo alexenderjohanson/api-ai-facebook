4f0afafb62e87cccade0cf18ef4c465661c6a718
Johanson Chew <alexenderjohanson@yahoo.com>
2016-05-23 05:59:24 +0800

    update

--- src/controller/billplz.js
+++ src/controller/billplz.js
@@ -9,28 +9,35 @@ const BILLPLZ_URL = `https://www.billplz.com/`;
 
 exports.generatePaymentLink = function (collectionId, name, email, contact, amount, desc) {
 
-    let query = {
-        collection_id: collectionId,
-        email: email,
-        mobile: contact,
-        name: name,
-        amount: amount,
-        callback_url: "https://www.google.com",
-        description: desc
+    try {
+        let query = {
+            collection_id: collectionId,
+            email: email,
+            mobile: contact,
+            name: name,
+            amount: amount,
+            callback_url: "https://www.google.com",
+            description: desc
+        }
+
+        let headers = {
+            Authorization: "Basic MGUxMjI5NWEtZGYyMi00MjQ3LTgyMWUtNzRlNjU5OTczNmQyOg=="
+        }
+
+        let queryString = qs.stringify(query);
+
+        let url = `${BILLPLZ_URL}api/v3/bills?${queryString}`;
+        
+        console.log(url);
+
+        return fetch(url, { method: 'POST', headers: headers })
+            .then(function (res) {
+                return res.json();
+            }, function (ex) {
+                console.log(ex);
+            });
+
+    } catch (ex) {
+        console.log(ex);
     }
-
-    let headers = {
-        Authorization: "Basic MGUxMjI5NWEtZGYyMi00MjQ3LTgyMWUtNzRlNjU5OTczNmQyOg=="
-    }
-
-    let queryString = qs.stringify(query);
-
-    let url = `${BILLPLZ_URL}api/v3/bills?${queryString}`;
-
-    return fetch(url, { method: 'POST', headers: headers })
-        .then(function (res) {
-            return res.json();
-        }, function (ex) {
-            console.log(ex);
-        });
 }
